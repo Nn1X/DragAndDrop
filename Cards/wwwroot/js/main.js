4ee931eb7cb09f316ceb5d488ed2989e3048ef03
Владислав Тюрин <aryrate@gmail.com>
2022-06-08 19:18:48 +0300

    Добавьте файлы проекта.

---
+++ Cards/wwwroot/js/main.js
@@ -0,0 +1,147 @@
+﻿
+
+
+function jsInit() {
+
+   
+
+    let draggetItem = null
+
+    const button = document.querySelector('.button')
+
+    function dragAndDrop() {
+        const listItems = document.querySelectorAll('.list__item')
+        const lists = document.querySelectorAll('.list')
+
+        for (let i = 0; i < listItems.length; i++) {
+
+            const item = listItems[i]
+            item.addEventListener('dragstart', () => {
+                draggetItem = item;
+                setTimeout(() => {
+                    item.style.display = 'none'
+                }, 0)
+            })
+
+            item.addEventListener('dragend', () => {
+                draggetItem = item;
+                setTimeout(() => {
+                    item.style.display = 'block'
+                    draggetItem = null
+                }, 0)
+            })
+
+            item.addEventListener('dblclick', () => {
+                item.remove()
+            })
+
+            for (let j = 0; j < lists.length; j++) {
+                const list = lists[j];
+
+                list.addEventListener('dragover', e => { e.preventDefault() })
+
+                list.addEventListener('dragenter', function (e) {
+                    e.preventDefault()
+                    this.style.backgroundColor = 'rgba(0,0,0, .3)'
+                })
+
+                list.addEventListener('dragleave', function (e) {
+                    e.preventDefault()
+                    this.style.backgroundColor = 'rgba(0,0,0, 0)'
+                })
+
+                list.addEventListener('drop', function (e) {
+                    this.style.backgroundColor = 'rgba(0,0,0, 0)'
+                    this.append(draggetItem);
+                })
+            }
+        }
+    }
+
+    dragAndDrop(draggetItem)
+
+    function addTask() {
+        const lists = document.querySelectorAll('.list')
+
+        const btn = document.querySelector('.add__btn')
+        const addBtn = document.querySelector('.add__item-btn')
+        const cancelBtn = document.querySelector('.cancel__item-btn')
+
+        const textarea = document.querySelector('.textarea')
+        const form = document.querySelector('.form')
+
+        let value
+
+        btn.addEventListener('click', () => {
+            form.style.display = 'block'
+            btn.style.display = 'none'
+            addBtn.style.display = 'none'
+
+            textarea.addEventListener('input', e => {
+                value = e.target.value
+
+                if (value) {
+                    addBtn.style.display = 'block'
+                } else{
+                    addBtn.style.display = 'none'
+                }
+            })
+        })
+
+        cancelBtn.addEventListener('click', () => {
+            textarea.value = ''
+            value = ''
+            form.style.display = 'none'
+            btn.style.display = 'flex'
+        })
+
+        addBtn.addEventListener('click', () => {
+            const newItem = document.createElement('div');
+            newItem.classList.add('list__item')
+            newItem.draggable = true
+            newItem.textContent = value;
+            lists[0].append(newItem);
+
+            form.style.display = 'none'
+            btn.style.display = 'flex'
+            textarea.value = ''
+            value = ''
+
+            dragAndDrop(draggetItem)
+        })
+    }
+
+    addTask()
+
+    function addBoard() {
+    
+
+        const boards = document.querySelector('.boards')
+
+        const board = document.createElement('div');
+        board.classList.add('boards__item')
+        board.innerHTML =`
+				    <span contenteditable="true" class="title">Введите название</span>
+                    <div class="list">
+			    </div>	`
+
+       boards.append(board);
+
+        changeTitle()
+
+        dragAndDrop(draggetItem)
+    }
+
+    button.addEventListener('click', addBoard)
+
+    function changeTitle() {
+        const titles = document.querySelectorAll('.title')
+
+        titles.forEach(title => {
+            title.addEventListener('click', e=>e.target.textContent = '')
+        })
+    }
+    changeTitle()
+   
+
+}
